42e05076d442d2377d9bdb9b1db4b3bf7259dc33
Mohammed Salmi <elmonstromail@gmail.com>
2022-10-26 11:33:29 -0400

    Make it works

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const panels = document.querySelectorAll(".panel");
+panels.forEach((panel) => {
+  panel.addEventListener("click", function () {
+    this.classList.toggle("open");
+    // this.classList.toggle("open-active");
+  });
+});
 
+panels.forEach((panel) => {
+  panel.addEventListener("transitionend", function (e) {
+    if (e.propertyName === "flex-grow") {
+      this.classList.toggle("open-active");
+    }
+  });
+});
